ef9f62882930bf9a7891ec6ce22c8f6ed5950568
swift1719 <ayushpawar1501@gmail.com>
2021-03-24 17:23:47 +0530

    modified utilites

--- src/board/board.js
+++ src/board/board.js
@@ -368,6 +368,20 @@ const getOppostiteDirection= direction=>{
     }
 }
 
+const getCellClassName=(cellValue,foodCell,foodShouldReverseDirection,snakeCells)=>{
+    let className='cell';
+    if(cellValue===foodCell){
+        if(foodShouldReverseDirection){
+            className ='cell cell-purple';
+        }else{
+            className='cell cell-red';
+        }
+    }
+    if(snakeCells.has(cellValue)){
+        className='cell cell-green';
+    }
+    return className;
+}
 
 
 export default Board;
