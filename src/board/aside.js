75efcbd01e128fe88aa250fc95730fa5f0e97110
swift1719 <ayushpawar1501@gmail.com>
2021-04-10 17:29:26 +0530

    added sidebars

---
+++ src/board/aside.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import snakeImg from '../assets/snake.png';
+const Aside = () => {
+    return (
+        <div className="aside">
+            <img width="200px" src={snakeImg} alt="snakeImage" />
+        </div>
+    )
+}
+export default Aside;
