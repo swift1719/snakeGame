c82af50d000a594cf240da0a8a43d59bf4c850f6
swift1719 <ayushpawar1501@gmail.com>
2021-04-10 18:02:34 +0530

    updated side bar view

--- src/board/aside.js
+++ src/board/aside.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import './aside.css';
+// import 'animate.css';
 import snakeImg from '../assets/snake.png';
 const Aside = () => {
     return (
         <div className="aside">
             <img width="200px" src={snakeImg} alt="snakeImage" />
+            <div className="main">
+                <h1 className="main-heading">
+                    <span className="main-heading-primary">Snake's</span><span className="main-heading-primary" > Quest</span>
+                    <span className="main-heading-secondary">Catch the food!!!</span>
+                </h1>
+            </div>
+            {/* <h1 class="animate__animated animate__bounce">An animated element</h1> */}
         </div>
     )
 }
